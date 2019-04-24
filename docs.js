7c5ab0b1b738d6dcd4982e2c882482029dea89b4
Daniel Scheid <Mac@Daniels-MacBook-Pro-3.local>
2019-04-23 19:49:41 -0700

    added more css

---
+++ docs.js
@@ -0,0 +1,22 @@
+
+// Add to the end of the array
+.push();
+
+//Pop the end of the array off
+.pop()
+
+// remove from middle of array, and how many elements
+.splice(index, count)
+
+// Turn a list into a string
+.join(' ');
+
+//Turns a string into a list
+.split(' ')
+
+//
+.sort();
+
+.forEach(function(food) {
+  console.log(food);
+})
