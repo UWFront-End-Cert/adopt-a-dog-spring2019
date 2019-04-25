22ee9ba9005d3d4b0cf49283e8cb9d95650fbce0
DAW <madronadan@gmail.com>
2019-04-25 13:21:40 -0700

    Created JS and made info pop ups for each dog

---
+++ aad.js
@@ -0,0 +1,59 @@
+function Murphy() {
+    alert("Murphy, Boxer, $123.45");
+  }
+
+
+function Poppy() {
+    alert("Poppy, Italian Anklebiter, $123.45");
+  }
+
+function Jack() {
+    alert("Jack, Gargantuan Dwarf Hound, $123.45")
+}
+
+function Duffy() {
+    alert("Duffy, Telepathic Cur, $123.45")
+}
+
+function Lucas() {
+    alert("Lucas, White-Nosed Goof, $123.45")
+}
+
+function Jake() {
+    alert("Jake, Blueblood Fetcher, $123.45")
+}
+
+function Angus() {
+    alert("Angus, Moordwelling Deathprowler, $123.45")
+}
+
+function Violet() {
+    alert("Violet, Lab, $123.45")
+}
+
+function Piper() {
+    alert("Piper, Scotish Shedder, $123.45")
+}
+
+function Maximus() {
+    alert("Maximus, Obstreperous Yowler, $123.45")
+}
+
+function Luna() {
+    alert("Luna, Dyson Clogger, $123.45")
+}
+
+function Stella() {
+    alert("Stella, Chiwawa, $123.45")
+}
+
+  let total = 0;
+  
+  function adder(value) {
+    let innerTotal = 0;
+    innerTotal += value;
+    total += value;
+    alert(total, 'total')
+  }
+
+  
