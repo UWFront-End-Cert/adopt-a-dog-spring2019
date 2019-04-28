3cb0d63d8061203b0b764667c12d297cdf28563c
georgeme <georgeme@uw.edu>
2019-04-27 20:59:01 -0700

    Adding js for image click

--- adopt.js
+++ adopt.js
@@ -1,58 +1,76 @@
 
 // Attempting to create alerts that are generic so it doesn't matter which dog image is clicked. Used IDs (removed from HTML for now) but since we don't have a breed element I couldn't figure out how to do this in time to turn in the assignment
-// function dogInfo() {
-//   let name = document.getElementById("dog-name").innerHTML;
-//   let cost = document.getElementById("dog-cost").innerHTML;
-//
-//   result = (name + ', ' + cost)
-//
-//   alert(result);
-// }
-
 function dogInfoMurphy() {
-  alert('Murphy' + ', ' + 'Boxer' + ', ' + '$123.45');
+  let name = document.getElementById("dog-name").innerHTML;
+  let breed = document.getElementById("breed").innerHTML;
+  // let cost = document.getElementById("dog-cost").innerHTML;
+
+  let result = (name + ', ' + breed);
+
+  alert(result);
 }
 
 function dogInfoPoppy() {
-  alert('Poppy' + ', ' + 'Puffball' + ', ' + '$123.45');
+  let name = document.getElementById("dog-name").innerHTML;
+  let breed = document.getElementById("breed").innerHTML;
+  // let cost = document.getElementById("dog-cost").innerHTML;
+
+  let result = (name + ', ' + breed);
+
+  alert(result);
 }
 
-function dogInfoJack() {
-  alert('Jack' + ', ' + 'Beagle' + ', ' + '$123.45');
-}
+// // Alerts with information for each dog
+// function dogInfoMurphy() {
+//   alert('Murphy' + ', ' + 'Boxer' + ', ' + '$123.45');
+// }
+//
+// function dogInfoPoppy() {
+//   alert('Poppy' + ', ' + 'Puffball' + ', ' + '$123.45');
+// }
+//
+// function dogInfoJack() {
+//   alert('Jack' + ', ' + 'Beagle' + ', ' + '$123.45');
+// }
+//
+// function dogInfoDuffy() {
+//   alert('Duffy' + ', ' + 'Bull Terrier' + ', ' + '$123.45');
+// }
+//
+// function dogInfoLucas() {
+//   alert('Lucas' + ', ' + 'Carolina Dog' + ', ' + '$123.45');
+// }
+//
+// function dogInfoJake() {
+//   alert('Jake' + ', ' + 'Retriever' + ', ' + '$123.45');
+// }
+//
+// function dogInfoAngus() {
+//   alert('Angus' + ', ' + 'Boxer' + ', ' + '$123.45');
+// }
+//
+// function dogInfoViolet() {
+//   alert('Violet' + ', ' + 'Puppers' + ', ' + '$123.45');
+// }
+//
+// function dogInfoPiper() {
+//   alert('Piper' + ', ' + 'Irish Setter' + ', ' + '$123.45');
+// }
+//
+// function dogInfoMaximus() {
+//   alert('Maximus' + ', ' + 'Angry Old Man' + ', ' + '$123.45');
+// }
+//
+// function dogInfoLuna() {
+//   alert('Luna' + ', ' + 'Golden Retriever' + ', ' + '$123.45');
+// }
+//
+// function dogInfoStella() {
+//   alert('Stella' + ', ' + 'Chihuahua' + ', ' + '$123.45');
+// }
 
-function dogInfoDuffy() {
-  alert('Duffy' + ', ' + 'Bull Terrier' + ', ' + '$123.45');
-}
+// Adopt button math
+function addCost() {
+  let
 
-function dogInfoLucas() {
-  alert('Lucas' + ', ' + 'Carolina Dog' + ', ' + '$123.45');
-}
-
-function dogInfoJake() {
-  alert('Jake' + ', ' + 'Retriever' + ', ' + '$123.45');
-}
-
-function dogInfoAngus() {
-  alert('Angus' + ', ' + 'Boxer' + ', ' + '$123.45');
-}
-
-function dogInfoViolet() {
-  alert('Violet' + ', ' + 'Puppers' + ', ' + '$123.45');
-}
-
-function dogInfoPiper() {
-  alert('Piper' + ', ' + 'Irish Setter' + ', ' + '$123.45');
-}
-
-function dogInfoMaximus() {
-  alert('Maximus' + ', ' + 'Angry Old Man' + ', ' + '$123.45');
-}
-
-function dogInfoLuna() {
-  alert('Luna' + ', ' + 'Golden Retriever' + ', ' + '$123.45');
-}
-
-function dogInfoStella() {
-  alert('Stella' + ', ' + 'Chihuahua' + ', ' + '$123.45');
 }
