130889fc44e83eb17cb4b363532562c240610831
georgeme <georgeme@uw.edu>
2019-04-28 10:11:54 -0700

    Updating js for dog info

--- adopt.js
+++ adopt.js
@@ -1,73 +1,135 @@
+function dogInfo1() {
 
-// Attempting to create alerts that are generic so it doesn't matter which dog image is clicked. Used IDs (removed from HTML for now) but since we don't have a breed element I couldn't figure out how to do this in time to turn in the assignment
-function dogInfoMurphy() {
-  let name = document.getElementById("dog-name").innerHTML;
-  let breed = document.getElementById("breed").innerHTML;
-  // let cost = document.getElementById("dog-cost").innerHTML;
+  // let name = dog.dog-name;
+  let name = document.getElementById("dog-name-1").innerHTML;
+  let breed = document.getElementById("breed-1").innerHTML;
 
-  let result = (name + ', ' + breed);
+  // result = (name + ', ' + breed);
 
-  alert(result);
+  alert(name + ', ' + breed);
 }
 
-function dogInfoPoppy() {
-  let name = document.getElementById("dog-name").innerHTML;
-  let breed = document.getElementById("breed").innerHTML;
-  // let cost = document.getElementById("dog-cost").innerHTML;
+function dogInfo2() {
 
-  let result = (name + ', ' + breed);
+  // let name = dog.dog-name;
+  let name = document.getElementById("dog-name-2").innerHTML;
+  let breed = document.getElementById("breed-2").innerHTML;
 
-  alert(result);
+  // result = (name + ', ' + breed);
+
+  alert(name + ', ' + breed);
 }
 
-// // Alerts with information for each dog
-// function dogInfoMurphy() {
-//   alert('Murphy' + ', ' + 'Boxer' + ', ' + '$123.45');
-// }
-//
-// function dogInfoPoppy() {
-//   alert('Poppy' + ', ' + 'Puffball' + ', ' + '$123.45');
-// }
-//
-// function dogInfoJack() {
-//   alert('Jack' + ', ' + 'Beagle' + ', ' + '$123.45');
-// }
-//
-// function dogInfoDuffy() {
-//   alert('Duffy' + ', ' + 'Bull Terrier' + ', ' + '$123.45');
-// }
-//
-// function dogInfoLucas() {
-//   alert('Lucas' + ', ' + 'Carolina Dog' + ', ' + '$123.45');
-// }
-//
-// function dogInfoJake() {
-//   alert('Jake' + ', ' + 'Retriever' + ', ' + '$123.45');
-// }
-//
-// function dogInfoAngus() {
-//   alert('Angus' + ', ' + 'Boxer' + ', ' + '$123.45');
-// }
-//
-// function dogInfoViolet() {
-//   alert('Violet' + ', ' + 'Puppers' + ', ' + '$123.45');
-// }
-//
-// function dogInfoPiper() {
-//   alert('Piper' + ', ' + 'Irish Setter' + ', ' + '$123.45');
-// }
-//
-// function dogInfoMaximus() {
-//   alert('Maximus' + ', ' + 'Angry Old Man' + ', ' + '$123.45');
-// }
-//
-// function dogInfoLuna() {
-//   alert('Luna' + ', ' + 'Golden Retriever' + ', ' + '$123.45');
-// }
-//
-// function dogInfoStella() {
-//   alert('Stella' + ', ' + 'Chihuahua' + ', ' + '$123.45');
-// }
+function dogInfo3() {
+
+  // let name = dog.dog-name;
+  let name = document.getElementById("dog-name-3").innerHTML;
+  let breed = document.getElementById("breed-3").innerHTML;
+
+  // result = (name + ', ' + breed);
+
+  alert(name + ', ' + breed);
+}
+
+function dogInfo4() {
+
+  // let name = dog.dog-name;
+  let name = document.getElementById("dog-name-4").innerHTML;
+  let breed = document.getElementById("breed-4").innerHTML;
+
+  // result = (name + ', ' + breed);
+
+  alert(name + ', ' + breed);
+}
+
+function dogInfo5() {
+
+  // let name = dog.dog-name;
+  let name = document.getElementById("dog-name-5").innerHTML;
+  let breed = document.getElementById("breed-5").innerHTML;
+
+  // result = (name + ', ' + breed);
+
+  alert(name + ', ' + breed);
+}
+
+function dogInfo6() {
+
+  // let name = dog.dog-name;
+  let name = document.getElementById("dog-name-6").innerHTML;
+  let breed = document.getElementById("breed-6").innerHTML;
+
+  // result = (name + ', ' + breed);
+
+  alert(name + ', ' + breed);
+}
+
+function dogInfo7() {
+
+  // let name = dog.dog-name;
+  let name = document.getElementById("dog-name-7").innerHTML;
+  let breed = document.getElementById("breed-7").innerHTML;
+
+  // result = (name + ', ' + breed);
+
+  alert(name + ', ' + breed);
+}
+
+function dogInfo8() {
+
+  // let name = dog.dog-name;
+  let name = document.getElementById("dog-name-8").innerHTML;
+  let breed = document.getElementById("breed-8").innerHTML;
+
+  // result = (name + ', ' + breed);
+
+  alert(name + ', ' + breed);
+}
+
+function dogInfo9() {
+
+  // let name = dog.dog-name;
+  let name = document.getElementById("dog-name-9").innerHTML;
+  let breed = document.getElementById("breed-9").innerHTML;
+
+  // result = (name + ', ' + breed);
+
+  alert(name + ', ' + breed);
+}
+
+function dogInfo10() {
+
+  // let name = dog.dog-name;
+  let name = document.getElementById("dog-name-10").innerHTML;
+  let breed = document.getElementById("breed-10").innerHTML;
+
+  // result = (name + ', ' + breed);
+
+  alert(name + ', ' + breed);
+}
+
+function dogInfo11() {
+
+  // let name = dog.dog-name;
+  let name = document.getElementById("dog-name-11").innerHTML;
+  let breed = document.getElementById("breed-11").innerHTML;
+
+  // result = (name + ', ' + breed);
+
+  alert(name + ', ' + breed);
+}
+
+function dogInfo12() {
+
+  // let name = dog.dog-name;
+  let name = document.getElementById("dog-name-12").innerHTML;
+  let breed = document.getElementById("breed-12").innerHTML;
+
+  // result = (name + ', ' + breed);
+
+  alert(name + ', ' + breed);
+}
+
 
 // Adopt button math
 function addCost() {
