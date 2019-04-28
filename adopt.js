122a69041187ca735477986cdd5f7128dd5bf390
georgeme <georgeme@uw.edu>
2019-04-27 19:52:22 -0700

    Updates to js and html for dog images

--- adopt.js
+++ adopt.js
@@ -1,4 +1,14 @@
 
+// Attempting to create alerts that are generic so it doesn't matter which dog image is clicked. Used IDs (removed from HTML for now) but since we don't have a breed element I couldn't figure out how to do this in time to turn in the assignment
+// function dogInfo() {
+//   let name = document.getElementById("dog-name").innerHTML;
+//   let cost = document.getElementById("dog-cost").innerHTML;
+//
+//   result = (name + ', ' + cost)
+//
+//   alert(result);
+// }
+
 function dogInfoMurphy() {
-  alert('Murphy');
+  alert('Murphy' + ', ' + 'Boxer' + ', ' + '$123.45');
 }
