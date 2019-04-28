35dbed95d1a82adcdda09c77122bed6b87b0a4f0
chanjessica <jchan_sea@yahoo.com>
2019-04-28 00:28:49 +0000

    remove <b> and user :first-child in CSS. add onclick event and functions

---
+++ scripts/adopt-dog.js
@@ -0,0 +1,18 @@
+let total=0;
+let count=0;
+function reset(){
+	total=0;
+	count=0;
+	console.log('total is ' + total);
+}
+function buyIt(price){
+	count ++;
+	total += price;
+	alert('Total number of dogs: '+ count + '\nYour total is $' + total);
+}
+function dogInfo(name,breed,cost){
+	let dogName = name.charAt(0).toUpperCase()+name.slice(1);
+	let log = dogName + " is  " + breed + "\nCost $" + cost + " to adopt.";
+	console.log(log);
+	alert(log);
+}
