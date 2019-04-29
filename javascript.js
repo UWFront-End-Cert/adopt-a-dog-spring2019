aafae4c62472ec4c792f2da5a1cdf9074f8cca96
ojspoko <ojspoko@gmail.com>
2019-04-28 21:24:41 -0400

    click handles for dog info

---
+++ javascript.js
@@ -0,0 +1,56 @@
+function Murphy() {
+    alert("Murphy, Hound Dog, $123.45");
+  }
+
+ function Poppy() {
+    alert("Poppy, Floof, $123.45");
+  }
+
+ function Jack() {
+    alert("Jack, Beagle, $123.45")
+}
+
+ function Duffy() {
+    alert("Duffy, Pit Bull, $123.45")
+}
+
+ function Lucas() {
+    alert("Lucas, Austrian Hound, $123.45")
+}
+
+ function Jake() {
+    alert("Jake, Yellow Lab, $123.45")
+}
+
+ function Angus() {
+    alert("Angus, Bull Mastiff, $123.45")
+}
+
+ function Violet() {
+    alert("Violet, Chocolate Lab, $123.45")
+}
+
+ function Piper() {
+    alert("Piper, Adventure Dog, $123.45")
+}
+
+ function Maximus() {
+    alert("Maximus, Havanese, $123.45")
+}
+
+ function Luna() {
+    alert("Luna, White Lab, $123.45")
+}
+
+ function Stella() {
+    alert("Stella, Chihuaha, $123.45")
+}
+
+   let total = 0;
+
+   function adder(value) {
+    let innerTotal = 0;
+    innerTotal += value;
+    total += value;
+    alert(total, 'total')
+  }
