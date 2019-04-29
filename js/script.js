967b09dd26d6a24ab53aa51fb67c7e7cef9b6fdb
aimzOG <aimz93@gmail.com>
2019-04-28 22:04:34 -0700

    adds js calculations for cart total

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
-function dogDescription(name, breed, price) {
-  alert(`${name}, ${breed}, ${price}`)
+let cartTotal = 0;
+
+function dogDescription(name, attribute, price) {
+  alert(`${name}, ${attribute}, $${price}`)
 }
 
+function addToTotal(price) {
+  cartTotal += price;
+  alert(`Your cart total is: $${cartTotal}`);
+}
