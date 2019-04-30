015db2590ba1601ff9f75b7794957cf23747ab5d
amycham <amycham@gmail.com>
2019-04-30 09:35:42 -0700

    image function and first event tested

---
+++ js/doginfo.js
@@ -0,0 +1,18 @@
+//global variables
+let feeTotal = 0;
+
+
+//image click function
+//pop alert with name, breed and fees
+function showInfo(dogname, dogbreed, dogfee){
+  alert(`${dogname} is a ${dogbreed}.\n${dogname}'s adoption fee is $${dogfee}.`);
+
+  return true;
+}
+
+//button (<a>) click function
+  //add adoption fee to feeTotal
+
+  //create dollar formatted string from feeTotal
+
+  //pop alert with formatted fee total
