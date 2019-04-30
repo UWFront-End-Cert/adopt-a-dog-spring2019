fb82bcf87a49751c0cc284e14294bb53220dcb34
georgeme <georgeme@uw.edu>
2019-04-29 19:15:34 -0700

    Updates to js formatting

--- adopt.js
+++ adopt.js
@@ -1,21 +1,21 @@
 // Function to alert dog info of name, breed, cost
 function dogInfo(imgEl) {
-
-	let parentArticleEl = imgEl.parentElement;
-	let dogNameEl = parentArticleEl.querySelector(".dog-name");
+  let parentArticleEl = imgEl.parentElement;
+  let dogNameEl = parentArticleEl.querySelector(".dog-name");
   let dogBreedEl = parentArticleEl.querySelector(".breed");
   let dogAmountEl = parentArticleEl.querySelector(".amount");
 
-	alert(dogNameEl.innerHTML + ', ' + dogBreedEl.innerHTML + ', ' + '$' + dogAmountEl.innerHTML);
+    alert(dogNameEl.innerHTML + ', ' + dogBreedEl.innerHTML + ', ' + '$' + dogAmountEl.innerHTML);
 }
 
 // Function to alert the cost when click on Adopt button, and provide added running total
 let totalPrice = 0;
+
 function addCost(buttonEl) {
   let parentArticleEl = buttonEl.parentElement;
   let amountEl = parentArticleEl.querySelector(".amount");
 
   totalPrice = totalPrice + parseFloat(amountEl.innerHTML);
 
-  alert(totalPrice);
+    alert(totalPrice);
 }
