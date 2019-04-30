1695f930d6acbcc7cb454e6db24828caf852ee27
Neha Patel <nehapatel0393@gmail.com>
2019-04-29 21:55:09 -0500

    click handlers for dogs added

---
+++ scripts/script.js
@@ -0,0 +1,49 @@
+//index.html & dogs.html click handlers for DOG PHOTOS
+
+function murphyInfo() {
+		alert("Murphy, Beagle, $123.45");
+}
+
+function poppyInfo() {
+		alert("Poppy, Beagle, $123.45");
+}
+
+function jackInfo() {
+		alert("Jack, Jack Russel Terrier, $123.45");
+}
+
+function duffyInfo() {
+		alert("Duffy, Pitbull, $123.45");
+}
+
+function lucasInfo() {
+		alert("Lucas, Beagle, $123.45");
+}
+
+function jakeInfo() {
+		alert("Jake, Labrador, $123.45");
+}
+
+function angusInfo() {
+		alert("Angus, Boxer, $123.45");
+}
+
+function violetInfo() {
+		alert("Violet, Chocolate Labrador, $123.45");
+}
+
+function piperInfo() {
+		alert("Piper, Chocolate Labrador, $123.45");
+}
+
+function maximusInfo() {
+		alert("Maximus, Chocolate Labrador, $123.45");
+}
+
+function lunaInfo() {
+		alert("Luna, Labrador, $123.45");
+}
+
+function stellaInfo() {
+		alert("Violet, Chihuahua, $123.45");
+}
