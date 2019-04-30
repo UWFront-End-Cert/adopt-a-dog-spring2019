770d311beb721521035d585d5454e2363306ac4d
georgeme <georgeme@uw.edu>
2019-04-29 19:00:14 -0700

    Updating js to use only one function

--- adopt.js
+++ adopt.js
@@ -1,118 +1,21 @@
-function dogInfo1() {
+// Function to alert dog info of name, breed, cost
+function dogInfo(imgEl) {
 
-// Alert the name, breed, and cost - hard coded breed so it doesn't show on the page (which breaks the mockup)
-  let name = document.getElementById("dog-name-1").innerHTML;
-  // let breed = document.getElementById("breed-1").innerHTML;
-  let amount = document.getElementById("amount-1").innerHTML;
+	let parentArticleEl = imgEl.parentElement;
+	let dogNameEl = parentArticleEl.querySelector(".dog-name");
+  let dogBreedEl = parentArticleEl.querySelector(".breed");
+  let dogAmountEl = parentArticleEl.querySelector(".amount");
 
-  alert(name + ', ' + 'Sad Face' + ', ' + amount);
+	alert(dogNameEl.innerHTML + ', ' + dogBreedEl.innerHTML + ', ' + '$' + dogAmountEl.innerHTML);
 }
 
-function dogInfo2() {
+// Function to alert the cost when click on Adopt button, and provide added running total
+let totalPrice = 0;
+function addCost(buttonEl) {
+  let parentArticleEl = buttonEl.parentElement;
+  let amountEl = parentArticleEl.querySelector(".amount");
 
-  let name = document.getElementById("dog-name-2").innerHTML;
-  // let breed = document.getElementById("breed-2").innerHTML;
-  let amount = document.getElementById("amount-2").innerHTML;
+  totalPrice = totalPrice + parseFloat(amountEl.innerHTML);
 
-  alert(name + ', ' + 'Puffball' + ', ' + amount);
-}
-
-function dogInfo3() {
-
-  let name = document.getElementById("dog-name-3").innerHTML;
-  // let breed = document.getElementById("breed-3").innerHTML;
-  let amount = document.getElementById("amount-3").innerHTML;
-
-  alert(name + ', ' + 'Beagle' + ', ' + amount);
-}
-
-function dogInfo4() {
-
-  let name = document.getElementById("dog-name-4").innerHTML;
-  // let breed = document.getElementById("breed-4").innerHTML;
-  let amount = document.getElementById("amount-4").innerHTML;
-
-  alert(name + ', ' + 'Bull Terrier' + ', ' + amount);
-}
-
-function dogInfo5() {
-
-  let name = document.getElementById("dog-name-5").innerHTML;
-  // let breed = document.getElementById("breed-5").innerHTML;
-  let amount = document.getElementById("amount-5").innerHTML;
-
-  alert(name + ', ' + 'Carolina Dog' + ', ' + amount);
-}
-
-function dogInfo6() {
-
-  let name = document.getElementById("dog-name-6").innerHTML;
-  // let breed = document.getElementById("breed-6").innerHTML;
-  let amount = document.getElementById("amount-6").innerHTML;
-
-  alert(name + ', ' + 'Retriever' + ', ' + amount);
-}
-
-function dogInfo7() {
-
-  let name = document.getElementById("dog-name-7").innerHTML;
-  // let breed = document.getElementById("breed-7").innerHTML;
-  let amount = document.getElementById("amount-7").innerHTML;
-
-  alert(name + ', ' + 'Boxer' + ', ' + amount);
-}
-
-function dogInfo8() {
-
-  let name = document.getElementById("dog-name-8").innerHTML;
-  // let breed = document.getElementById("breed-8").innerHTML;
-  let amount = document.getElementById("amount-8").innerHTML;
-
-  alert(name + ', ' + 'Puppers' + ', ' + amount);
-}
-
-function dogInfo9() {
-
-  let name = document.getElementById("dog-name-9").innerHTML;
-  // let breed = document.getElementById("breed-9").innerHTML;
-  let amount = document.getElementById("amount-9").innerHTML;
-
-  alert(name + ', ' + 'Irish Setter' + ', ' + amount);
-}
-
-function dogInfo10() {
-
-  let name = document.getElementById("dog-name-10").innerHTML;
-  // let breed = document.getElementById("breed-10").innerHTML;
-  let amount = document.getElementById("amount-10").innerHTML;
-
-  alert(name + ', ' + 'Angry Old Man' + ', ' + amount);
-}
-
-function dogInfo11() {
-
-  let name = document.getElementById("dog-name-11").innerHTML;
-  // let breed = document.getElementById("breed-11").innerHTML;
-  let amount = document.getElementById("amount-11").innerHTML;
-
-  alert(name + ', ' + 'Golden Retriever' + ', ' + amount);
-}
-
-function dogInfo12() {
-
-  let name = document.getElementById("dog-name-12").innerHTML;
-  // let breed = document.getElementById("breed-11").innerHTML;
-  let amount = document.getElementById("amount-12").innerHTML;
-
-  alert(name + ', ' + 'Chihuahua' + ', ' + amount);
-}
-
-
-// Adopt button math
-let costDog = new Array();
-costDog["dog1"]=300;
-costDog["dog2"]=500;
-
-function addCost() {
-
+  alert(totalPrice);
 }
