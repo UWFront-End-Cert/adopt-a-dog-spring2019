d549200220f45b3df670c1fbbfd436d0d15ad450
Cwest <46390605+caitlinwest10@users.noreply.github.com>
2019-04-29 22:33:33 -0500

    Correcting js file and html based on James discussion comments

--- script.js
+++ script.js
@@ -2,52 +2,8 @@ let adoptionFee = 123.45;
 
 let totalPrice = 0;
 
-        function murphyClick() {
-            alert("Are you ready to take Murphy home? Adoption is $123.45");
-        }
-
-        function poppyClick() {
-            alert("Are you ready to take Poppy home? Adoption is $123.45");
-        }
-
-        function jackClick() {
-            alert("Are you ready to take Jack home? Adoption is $123.45");
-        }
-
-        function duffyClick() {
-            alert("Are you ready to take Duffy home? Adoption is $123.45");
-        }
-
-        function lucasClick() {
-            alert("Are you ready to take Lucas home? Adoption is $123.45");
-        }
-
-        function jakeClick() {
-            alert("Are you ready to take Jake home? Adoption is $123.45");
-        }
-
-        function angusClick() {
-            alert("Are you ready to take Angus home? Adoption is $123.45");
-        }
-
-        function piperClick() {
-            alert("Are you ready to take Piper home? Adoption is $123.45");
-        }
-
-        function maximusClick() {
-            alert("Are you ready to take Maximus home? Adoption is $123.45");
-        }
-
-        function lunaClick() {
-            alert("Are you ready to take Luna home? Adoption is $123.45");
-        }
-
-        function stellaClick() {
-            alert("Are you ready to take Stella home? Adoption is $123.45");
-        }
-
-        function violetClick() {
-            alert("Are you ready to take Violet home? Adoption is $123.45");
+        function dogClick(data) {
+            alert(data);
         }
 
         function adoptionClick() {
