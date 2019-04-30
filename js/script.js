a758841667b1faff91b4f521b5ace9691b4c49fe
Ian Smith <yuppiecruncher@gmail.com>
2019-04-30 15:15:00 -0700

    Incorporated JS into index.html and dogs.html created js directory and file for reference and management.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+function doggoInfo(name, cost) {
+  alert('Name: ' + name + '\n' + 'Cost: ' + cost);
+}
+
+let total = 0
+
+function doggoCost(cost) {
+  total = total + Number(cost);
+  alert('$ '+ total)
+}
