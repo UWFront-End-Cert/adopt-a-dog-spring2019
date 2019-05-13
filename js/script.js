caab2637fdb1f9e7159e405f6edf0547b582b714
aimzOG <aimz93@gmail.com>
2019-05-13 01:21:38 -0700

    adds blog preview, adjusts media queries

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 let cartTotal = 0;
 
+// describes dog when img is clicked
 function dogDescription(name, attribute, price) {
   alert(`${name}, ${attribute}, $${price}`)
 }
 
+// calculates running total of price of all dogs in cart
 function addToTotal(price) {
   cartTotal += price;
   alert(`Your cart total is: $${cartTotal}`);
