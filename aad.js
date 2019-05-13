1afc1c62c81efaa6d883a338820142e353e2e4d6
DAW <madronadan@gmail.com>
2019-05-13 13:42:54 -0700

    Finished final styling, made js more streamlined, inserted media queries for sizing

--- aad.js
+++ aad.js
@@ -1,50 +1,7 @@
-function Murphy() {
-    alert("Murphy, Boxer, $123.45");
+function info(name, breed) {
+    let result = name + ', ' + breed + ', $123.45';
+    alert(result);
   }
-
-function Poppy() {
-    alert("Poppy, Italian Anklebiter, $123.45");
-  }
-
-function Jack() {
-    alert("Jack, Gargantuan Dwarf Hound, $123.45")
-}
-
-function Duffy() {
-    alert("Duffy, Telepathic Cur, $123.45")
-}
-
-function Lucas() {
-    alert("Lucas, White-Nosed Goof, $123.45")
-}
-
-function Jake() {
-    alert("Jake, Blueblood Fetcher, $123.45")
-}
-
-function Angus() {
-    alert("Angus, Moordwelling Deathprowler, $123.45")
-}
-
-function Violet() {
-    alert("Violet, Lab, $123.45")
-}
-
-function Piper() {
-    alert("Piper, Scotish Shedder, $123.45")
-}
-
-function Maximus() {
-    alert("Maximus, Obstreperous Yowler, $123.45")
-}
-
-function Luna() {
-    alert("Luna, Dyson Clogger, $123.45")
-}
-
-function Stella() {
-    alert("Stella, Chiwawa, $123.45")
-}
 
 let total = 0;
   
