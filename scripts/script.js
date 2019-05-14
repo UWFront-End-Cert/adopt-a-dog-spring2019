f21fb43de4a49eb200023ceda5a7fa8b4d006a15
Neha Patel <nehapatel0393@gmail.com>
2019-05-13 22:23:30 -0500

    dog cards for mobile and tablet

--- scripts/script.js
+++ scripts/script.js
@@ -55,3 +55,20 @@ function adoptAlert() {
 		total += 123.45;
 	alert('This dog has been added to your cart. Total: $' + total);
 }
+
+//More buttons
+function buttonMore () {
+	var dots = document.getElementById("initial-dog-cards");
+	var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
