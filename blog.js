7518d7a9c8f532c0504b859702f00c529565fadc
Cwest <46390605+caitlinwest10@users.noreply.github.com>
2019-05-23 17:45:44 -0500

    Additional JS changes

--- blog.js
+++ blog.js
@@ -1,33 +1,35 @@
+const wrap = document.getElementById('wrapper');
+
 /* Blog 1 */
 
 const travel = document.createElement('article');
 
-travel.innerHTML = '<img src="images/blog-1.jpg">'
-                    + '<h3>Traveling With Your Dog</h3>'
-                    + '<p>Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.</p>'
-                    + '<p>Uptiusd andesci qui nem aut vendion ectur ? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.</p>';
+travel.innerHTML = '<img src="images/blog-1.jpg" alt="Picture of a        dog and its owner looking at the scenery">' +
+    '<h3>Traveling With Your Dog</h3>' +
+    '<p>Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.</p>' +
+    '<p>Uptiusd andesci qui nem aut vendion ectur ? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.</p>';
 travel.setAttribute('class', 'blog-entry');
-
-const wrap = document.getElementById('wrapper');
 
 wrap.appendChild(travel);
 
 /* Blog 2 */
 
 const walk = document.createElement('article');
-walk.innerHTML = '<img src="images/blog-2.jpg">'
-                    + '<h3>How to Walk Multiple Dogs</h3>'
-                    + '<p>Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.</p>'
-                    + '<p>Uptiusd andesci qui nem aut vendion ectur ? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.</p>';
+walk.innerHTML = '<img src="images/blog-2.jpg" alt="Picture of            several dogs on their leashes at the park">' +
+    '<h3>How to Walk Multiple Dogs</h3>' +
+    '<p>Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.</p>' +
+    '<p>Uptiusd andesci qui nem aut vendion ectur ? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.</p>';
 walk.setAttribute('class', 'blog-entry');
+
 wrap.appendChild(walk);
 
 /* Blog 3 */
 
 const fetch = document.createElement('article');
-fetch.innerHTML = '<img src="images/blog-3.jpg">'
-    + '<h3>Teach Your Dog To Fetch!</h3>'
-    + '<p>Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.</p>'
-    + '<p>Uptiusd andesci qui nem aut vendion ectur ? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.</p>';
+fetch.innerHTML = '<img src="images/blog-3.jpg" alt="Picture of a        person about to throw a ball for their dog">' +
+    '<h3>Teach Your Dog To Fetch!</h3>' +
+    '<p>Iduciendisite quo magnatem iuntum quid quaest ea am, tenderumet adis dolenem quidustrum fuga. Faceaquae estioria derum recuptatur, cum volore, undipsa doloreium hillupta aut es ut alitatuscit ommossum haritatur arum qui officae videbiti corporeium faccull oribus es quidignis ipietus explam sus am aut amet ant fugiatum, utem non reptat.</p>' +
+    '<p>Uptiusd andesci qui nem aut vendion ectur ? Debis que explaut laborenia que doluptur, con et labor abor sant poreperum dio quat que doluptatur aut voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam voluptassim quisciatquam ea ad qui con nes cus esere dolut hicto teni solutenis alit ulparume nonseca estorer spernam.</p>';
 fetch.setAttribute('class', 'blog-entry');
+
 wrap.appendChild(fetch);
