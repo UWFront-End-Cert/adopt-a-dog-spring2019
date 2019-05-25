9f14262d184023fb30455c470d4d461f5cedfc25
chanjessica <43660859+chanjessica@users.noreply.github.com>
2019-05-25 00:47:20 -0700

    Create contact.js
    alert & console log

---
+++ scripts/contact.js
@@ -0,0 +1,19 @@
+function sConsole(event) {
+    alert('Thank you! Your information has been received');
+    event.preventDefault();
+    console.log ( 'name   : ' + document.getElementById('name').value );
+  	console.log ( 'email  : ' + document.getElementById('email').value );  
+	  console.log ( 'address: ' + document.getElementById('address').value );
+	  console.log ( 'city   : ' + document.getElementById('city').value );
+	  console.log ( 'state  : ' + document.getElementById('state').value );
+	  console.log ( 'zip    : ' + document.getElementById('zip').value );
+    console.log ( 'Pickup location    : ' + document.getElementById('loc').value );
+    console.log ( 'First time adopted?') ;
+    if (document.getElementById('firstYes').checked) {
+        console.log(document.getElementById('firstYes').value);
+        }
+    else if (document.getElementById('firstNo').checked) {
+        console.log(document.getElementById('firstNo').value);
+        };   
+  }
+ 
