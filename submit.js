d5a101e26a60028ee0da33620f9fdcbca0c0e758
georgeme <georgeme@uw.edu>
2019-05-25 20:46:00 -0700

    Adding JS for submit functions

--- submit.js
+++ submit.js
@@ -1,3 +1,8 @@
+let form = document.forms[0];
+let selectName = document.querySelector('input[name="contactName"]');
+let formName = selectName.value;
+  console.log(formName);
+
 function submitAlert() {
-  alert('Thank you! The information has been received.')
+  alert('Thank you! The information has been received.');
 }
