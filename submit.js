dd38ba96ef254bbc9e6022e882ad27d50fb2583a
georgeme <georgeme@uw.edu>
2019-05-25 21:42:59 -0700

    Add JS to console log form input values

--- submit.js
+++ submit.js
@@ -1,8 +1,27 @@
-let form = document.forms[0];
-let selectName = document.querySelector('input[name="contactName"]');
-let formName = selectName.value;
-  console.log(formName);
 
 function submitAlert() {
-  alert('Thank you! The information has been received.');
+
+  let form = document.forms[0];
+
+  let selectName = document.querySelector('input[name="contactName"]');
+  let formName = selectName.value;
+    console.log('Name: ' + formName);
+
+  let selectEmail = document.querySelector('input[name="contactEmail"]');
+  let formEmail = selectEmail.value;
+    console.log('Email: ' + formEmail);
+
+  let selectAddress = document.querySelector('input[name="contactStreet"]');
+  let formAddress = selectAddress.value;
+    console.log('Address: ' + formAddress);
+
+  let selectCity = document.querySelector('input[name="contactCity"]');
+  let formCity = selectCity.value;
+    console.log('City: ' + formCity);
+
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+  });
+
+      alert('Thank you! The information has been received.');
 }
