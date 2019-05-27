576792216479096d4abb8d4fc45e956c45089791
Neha Patel <nehapatel0393@gmail.com>
2019-05-27 18:13:57 -0500

    validate email, console log, alert from submit on contact.html

--- scripts/script.js
+++ scripts/script.js
@@ -103,12 +103,99 @@ blogEntry1.appendChild(blogEntry1Header);
 blogEntry1.appendChild(blogEntry1Content);
 
 //Blog Entry 2
+const blogEntry2 = document.createElement('div');
+blogEntry2.innerHTML = '<h1>Entry 2</h1>';
+blogEntry2.setAttribute('class', 'entry');
+
+console.log(blogEntry1);
+
+const blogEntry2Img = document.createElement('img');
+blogEntry2Img.setAttribute('src', 'images/blog-2.jpg');
+blogEntry2Img.setAttribute("alt", "group of dogs being walked");
+blogEntry2Img.setAttribute('class', 'blog-picture');
+
+const blogEntry2Header = document.createElement('h3');
+blogEntry2Header.textcontent = 'Lorem Ipsum';
+
+const blogEntry2Content = document.createElement('p');
+blogEntry2Content.textcontent = "Lorem ipsum";
+blogEntry2Img.setAttribute('class', 'blog-text');
+
+blogEntry2.appendChild(blogEntry2Img);
+blogEntry2.appendChild(blogEntry2Header);
+blogEntry2.appendChild(blogEntry2Content);
 
 //Blog Entry 3
+const blogEntry1 = document.createElement('div');
+blogEntry1.innerHTML = '<h1>Entry 1</h1>';
+blogEntry1.setAttribute('class', 'entry');
 
+console.log(blogEntry1);
+
+const blogEntry1Img = document.createElement('img');
+blogEntry1Img.setAttribute('src', 'images/blog-1.jpg');
+blogEntry1Img.setAttribute("alt", "girl with dog overlooking the Grand Canyon");
+blogEntry1Img.setAttribute('class', 'blog-picture');
+
+const blogEntry1Header = document.createElement('h3');
+blogEntry1Header.textcontent = 'Lorem Ipsum';
+
+const blogEntry1Content = document.createElement('p');
+blogEntry1Content.textcontent = "Lorem ipsum";
+blogEntry1Img.setAttribute('class', 'blog-text');
+
+blogEntry1.appendChild(blogEntry1Img);
+blogEntry1.appendChild(blogEntry1Header);
+blogEntry1.appendChild(blogEntry1Content);
+
+//Blog Entry 2
+const blogEntry3 = document.createElement('div');
+blogEntry3.innerHTML = '<h1>Entry 3</h1>';
+blogEntry3.setAttribute('class', 'entry');
+
+console.log(blogEntry1);
+
+const blogEntry3Img = document.createElement('img');
+blogEntry3Img.setAttribute('src', 'images/blog-3.jpg');
+blogEntry3Img.setAttribute("alt", "illouette of little girl playing with a dog");
+blogEntry3Img.setAttribute('class', 'blog-picture');
+
+const blogEntry3Header = document.createElement('h3');
+blogEntry3Header.textcontent = 'Lorem Ipsum';
+
+const blogEntry3Content = document.createElement('p');
+blogEntry3Content.textcontent = "Lorem ipsum";
+blogEntry3Img.setAttribute('class', 'blog-text');
+
+blogEntry3.appendChild(blogEntry3Img);
+blogEntry3.appendChild(blogEntry3Header);
+blogEntry3.appendChild(blogEntry3Content);
 
 for (let i = 0; i < blogArr.length; i++) {
 	el.appendChild(blogArr[i]); //last thing you want. will loop over thing arr; everything and add it to this element
 }
 
-//Alert for form & Console log the values inside the form, you can build a string or build an object.
+//FORM
+
+function show_alert() {
+	alert("The form has been submitted.");
+}//Alert for form & Console log the values inside the form, you can build a string or build an object.
+
+console.log({text:'' email:''});
+
+//Validate email
+function emailValid (inputText) {
+	var email = document.getElementById('txtEmail');
+	var filter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+	if(inputText.value.match(email.value)) {
+		email.focus();
+	return true;
+	}
+
+	else {
+		alert("Please enter a valid email address.");
+		email.focus();
+		return false;
+	}
+} //Code c/o https://www.codeproject.com/Tips/492632/Email-Validation-in-JavaScript
