d23d3f85d669d27144f943f584386f17a5610b93
Steve Fountain (HARMAN Connected Services) <v-sfoun@microsoft.com>
2019-05-27 23:04:34 -0700

    update to checkout scripts

--- javascript/scripts-checkout.js
+++ javascript/scripts-checkout.js
@@ -8,23 +8,67 @@ submitButton.setAttribute('class', 'adopt-button-checkout');
 submitButton.textContent = 'Submit';
 form.appendChild(submitButton);
 
+
+
+form.addEventListener('submit', function(event) {
+
 const name = document.getElementById('name');
 const email = document.getElementById('email');
 const address = document.getElementById('address');
 const city = document.getElementById('city');
 const state = document.getElementById('state');
 const zip = document.getElementById('zip');
-const yesFirst = document.getElementById('yes-first');
-const noFirst = document.getElementById('no-first');
+const adopter = adopt();
 const pickupLocation = document.getElementById('pickup-location');
 
+function adopt() {
+    if (document.getElementById('yes-first').checked) {
+        return document.getElementById('yes-first');
+      }
+    else if (document.getElementById('no-first').checked) {
+        return document.getElementById('no-first');
+    }
+}
 
-let customerInfo = [name, email, address, city, state, zip, yesFirst, noFirst, pickupLocation];
+let customerInfo = [
+    {
+        title: 'name',
+        value: name.value
+    },
+    {
+        title: 'email',
+        value: email.value
+    },
+    {
+        title: 'address',
+        value: address.value
+    },
+    {
+        title: 'city',
+        value: city.value
+    },
+    {
+        title: 'state',
+        value: state.value
+    },
+    {
+        title: 'zip',
+        value: zip.value
+    },
+    {
+        title: 'First time adopter?',
+        value: adopter.value
+    },
+    {
+        title: 'Pickup Location?',
+        value: pickupLocation.value
+    }
+];
 
-form.addEventListener('submit', function(event) {
     event.preventDefault();
     for ( i = 0; i < customerInfo.length; i +=1) {
-        console.log(customerInfo[i].value);
+        console.log(customerInfo[i].title + ': ' + customerInfo[i].value);
     }
     alert('Thank you. The form information has been received');
+
 });
