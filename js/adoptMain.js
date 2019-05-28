808142efd613f8922c2aaedc9035d4bdfaada73a
Lucas Knezevich <knezevich.lucas@gmail.com>
2019-05-27 18:07:30 -0700

    Created js for all three blog posts

--- js/adoptMain.js
+++ js/adoptMain.js
@@ -28,16 +28,15 @@ function addToCart(name, dogCost) {
 
 
 //  BLOG POSTS  //
-const blogSection = document.getElementById('blog-main');
-
-const blogContent = [];
+const blogSection = document.getElementsByClassName('blog-main');
+console.log(blogSection);
 
 // 1st Post
 const blogPostA = document.createElement('article');
 blogPostA.setAttribute('class', 'blog-post');
 
 const imageA = document.createElement('img');
-imageA.setAttribute('src', 'https://picsum.photos/200');
+imageA.setAttribute('src', './images/blog-1.jpg');
 imageA.setAttribute('alt', 'Woman and dog sitting at the Grand Canyon');
 
 const postAContent = document.createElement('div');
@@ -58,3 +57,64 @@ postAContent.appendChild(postAAside);
 
 blogPostA.appendChild(imageA);
 blogPostA.appendChild(postAContent);
+
+console.log(blogPostA);
+
+// 2nd Post
+const blogPostB = document.createElement('article');
+blogPostB.setAttribute('class', 'blog-post');
+
+const imageB = document.createElement('img');
+imageB.setAttribute('src', './images/blog-2.jpg');
+imageB.setAttribute('alt', 'Multiple dogs sitting, eagerly awaiting something');
+
+const postBContent = document.createElement('div');
+postBContent.setAttribute('class', 'blog-content');
+
+const postBTitle = document.createElement('h3');
+postBTitle.textContent = 'Traveling With Your Dog';
+
+const postBPar = document.createElement('p');
+postBPar.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+const postBAside = document.createElement('aside');
+postBAside.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+postBContent.appendChild(postBTitle);
+postBContent.appendChild(postBPar);
+postBContent.appendChild(postBAside);
+
+blogPostB.appendChild(imageB);
+blogPostB.appendChild(postBContent);
+
+console.log(blogPostB);
+
+// 3rd Post
+const blogPostC = document.createElement('article');
+blogPostC.setAttribute('class', 'blog-post');
+
+const imageC = document.createElement('img');
+imageC.setAttribute('src', './images/blog-3.jpg');
+imageC.setAttribute('alt', 'Multiple dogs sitting, eagerly awaiting something');
+
+const postCContent = document.createElement('div');
+postCContent.setAttribute('class', 'blog-content');
+
+const postCTitle = document.createElement('h3');
+postCTitle.textContent = 'Traveling With Your Dog';
+
+const postCPar = document.createElement('p');
+postCPar.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+const postCAside = document.createElement('aside');
+postCAside.textContent = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+
+postCContent.appendChild(postCTitle);
+postCContent.appendChild(postCPar);
+postCContent.appendChild(postCAside);
+
+blogPostC.appendChild(imageC);
+blogPostC.appendChild(postCContent);
+
+console.log(blogPostC);
+
