38b14cb2420588138979c46d90e4123bfac3186b
DAW <madronadan@gmail.com>
2019-05-28 09:10:22 -0700

    form working but not radio buttons; blog text and titles at bottom, no images

--- aad.js
+++ aad.js
@@ -12,4 +12,17 @@ function adder(value) {
     alert(total)
 }
 
-  
+function thankYou() {
+  event.preventDefault();
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let street = document.getElementById("street").value;
+  let city = document.getElementById("city").value;
+  let state = document.getElementById("state").value;
+  let zip = document.getElementById("zip").value;
+  let first = document.getElementById("first").value;
+  let pickup = document.getElementById("pickup").value;
+  let myArr = [name, email, street, city, state, zip, first, pickup]
+  console.log(myArr);
+  alert("Thank you. The form information has been received.");
+}
