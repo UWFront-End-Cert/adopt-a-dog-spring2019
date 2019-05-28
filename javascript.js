05ed6084c0f4b692345da2e0f44865481ff4749f
ojspoko <ojspoko@gmail.com>
2019-05-27 21:48:58 -0400

    lesson 8 js files

--- javascript.js
+++ javascript.js
@@ -11,6 +11,17 @@ let total = 0;
     alert('Grand total: ' + '$' + total)
   }
 
+function submitValues() {
+    console.log(document.getElementById('name').value);
+    console.log(document.getElementById('email').value);
+    console.log(document.getElementById('address').value);
+    console.log(document.getElementById('city').value);
+    console.log(document.getElementById('state').value);
+    console.log(document.getElementById('zip').value);
+    console.log(document.getElementById('location').value);
+    console.log(document.getElementById('first_adoption').value);
+  };
+
 function submitForm() {
   alert('Thank you. The form information has been received');
 }
