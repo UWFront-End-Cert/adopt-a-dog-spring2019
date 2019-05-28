dbe3683c74620c2b794ac07a62d5b694b569dca5
Neha Patel <nehapatel0393@gmail.com>
2019-05-27 21:45:25 -0500

    fixed some bugs

--- scripts/script.js
+++ scripts/script.js
@@ -107,7 +107,7 @@ const blogEntry2 = document.createElement('div');
 blogEntry2.innerHTML = '<h1>Entry 2</h1>';
 blogEntry2.setAttribute('class', 'entry');
 
-console.log(blogEntry1);
+console.log(blogEntry2);
 
 const blogEntry2Img = document.createElement('img');
 blogEntry2Img.setAttribute('src', 'images/blog-2.jpg');
@@ -126,38 +126,15 @@ blogEntry2.appendChild(blogEntry2Header);
 blogEntry2.appendChild(blogEntry2Content);
 
 //Blog Entry 3
-const blogEntry1 = document.createElement('div');
-blogEntry1.innerHTML = '<h1>Entry 1</h1>';
-blogEntry1.setAttribute('class', 'entry');
-
-console.log(blogEntry1);
-
-const blogEntry1Img = document.createElement('img');
-blogEntry1Img.setAttribute('src', 'images/blog-1.jpg');
-blogEntry1Img.setAttribute("alt", "girl with dog overlooking the Grand Canyon");
-blogEntry1Img.setAttribute('class', 'blog-picture');
-
-const blogEntry1Header = document.createElement('h3');
-blogEntry1Header.textcontent = 'Lorem Ipsum';
-
-const blogEntry1Content = document.createElement('p');
-blogEntry1Content.textcontent = "Lorem ipsum";
-blogEntry1Img.setAttribute('class', 'blog-text');
-
-blogEntry1.appendChild(blogEntry1Img);
-blogEntry1.appendChild(blogEntry1Header);
-blogEntry1.appendChild(blogEntry1Content);
-
-//Blog Entry 2
 const blogEntry3 = document.createElement('div');
-blogEntry3.innerHTML = '<h1>Entry 3</h1>';
+blogEntry3.innerHTML = '<h1>Entry 1</h1>';
 blogEntry3.setAttribute('class', 'entry');
 
-console.log(blogEntry1);
+console.log(blogEntry3);
 
 const blogEntry3Img = document.createElement('img');
 blogEntry3Img.setAttribute('src', 'images/blog-3.jpg');
-blogEntry3Img.setAttribute("alt", "illouette of little girl playing with a dog");
+blogEntry3Img.setAttribute("alt", "girl with dog overlooking the Grand Canyon");
 blogEntry3Img.setAttribute('class', 'blog-picture');
 
 const blogEntry3Header = document.createElement('h3');
@@ -175,13 +152,11 @@ for (let i = 0; i < blogArr.length; i++) {
 	el.appendChild(blogArr[i]); //last thing you want. will loop over thing arr; everything and add it to this element
 }
 
-//FORM
+//Contact Forms
 
 function show_alert() {
 	alert("The form has been submitted.");
 }//Alert for form & Console log the values inside the form, you can build a string or build an object.
-
-console.log({text:'' email:''});
 
 //Validate email
 function emailValid (inputText) {
@@ -199,3 +174,5 @@ function emailValid (inputText) {
 		return false;
 	}
 } //Code c/o https://www.codeproject.com/Tips/492632/Email-Validation-in-JavaScript
+
+//Checkout Forms
