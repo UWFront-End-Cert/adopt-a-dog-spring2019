b19ce6fb9595ebf2230961d9cfdd01cb44565a4b
ojspoko <ojspoko@gmail.com>
2019-05-27 20:52:28 -0400

    simplified javascript file by condensing to a single function

--- javascript.js
+++ javascript.js
@@ -1,50 +1,6 @@
-function Murphy() {
-    alert("Murphy, Hound Dog, $150.89");
+function dogInfo () {
+    alert();
   }
-
- function Poppy() {
-    alert("Poppy, Floof, $123.45");
-  }
-
- function Jack() {
-    alert("Jack, Beagle, $100.00")
-}
-
- function Duffy() {
-    alert("Duffy, Pit Bull, $123.45")
-}
-
- function Lucas() {
-    alert("Lucas, Austrian Hound, $123.45")
-}
-
- function Jake() {
-    alert("Jake, Yellow Lab, $123.45")
-}
-
- function Angus() {
-    alert("Angus, Bull Mastiff, $123.45")
-}
-
- function Violet() {
-    alert("Violet, Chocolate Lab, $123.45")
-}
-
- function Piper() {
-    alert("Piper, Adventure Dog, $123.45")
-}
-
- function Maximus() {
-    alert("Maximus, Havanese, $123.45")
-}
-
- function Luna() {
-    alert("Luna, White Lab, $123.45")
-}
-
-  function Stella() {
-    alert("Stella, Chihuaha, $123.45")
-}
 
 let total = 0;
 
