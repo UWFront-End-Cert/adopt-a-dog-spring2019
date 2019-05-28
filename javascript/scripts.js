8047d2c472b7201411424a6b3ae88dced19ab399
Steve Fountain (HARMAN Connected Services) <v-sfoun@microsoft.com>
2019-05-27 21:51:46 -0700

    style updates

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -10,37 +10,6 @@ function addToCart(fee) {
     return cartTotal;
 }
 
-//checkout page
-
-const form = document.querySelector('form');
-const submitButton = document.createElement('button');
-submitButton.setAttribute('type', 'submit');
-
-submitButton.textContent = 'Submit';
-form.appendChild(submitButton);
-
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const address = document.getElementById('address');
-const city = document.getElementById('city');
-const state = document.getElementById('state');
-const zip = document.getElementById('zip');
-const yesFirst = document.getElementById('yes-first');
-const noFirst = document.getElementById('no-first');
-const pickupLocation = document.getElementById('pickup-location');
-
-
-let customerInfo = [name, email, address, city, state, zip, yesFirst, noFirst, pickupLocation];
-
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    for ( i = 0; i < customerInfo.length; i +=1) {
-        console.log(    customerInfo[i].value);
-    }
-    alert('Thank you. The form information has been received');
-});
-
-
 // blog page
 
 const section = document.createElement('section');
@@ -109,5 +78,38 @@ for (let i = 0; i < blog.length; i += 1) {
     document.getElementsByClassName("blog-post")[i].appendChild(p1);
     document.getElementsByClassName("blog-post")[i].appendChild(p2);
 }
+
+
+//checkout page
+
+const form = document.querySelector('form');
+const submitButton = document.createElement('button');
+submitButton.setAttribute('type', 'submit');
+
+submitButton.textContent = 'Submit';
+form.appendChild(submitButton);
+
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const address = document.getElementById('address');
+const city = document.getElementById('city');
+const state = document.getElementById('state');
+const zip = document.getElementById('zip');
+const yesFirst = document.getElementById('yes-first');
+const noFirst = document.getElementById('no-first');
+const pickupLocation = document.getElementById('pickup-location');
+
+
+let customerInfo = [name, email, address, city, state, zip, yesFirst, noFirst, pickupLocation];
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    for ( i = 0; i < customerInfo.length; i +=1) {
+        console.log(customerInfo[i].value);
+    }
+    alert('Thank you. The form information has been received');
+});
+
+
 
 
