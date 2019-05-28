d2191eb940d6e96f4f4a82bef009acdd2f3d2513
DAW <madronadan@gmail.com>
2019-05-28 09:54:33 -0700

    js for blog

---
+++ blog.js
@@ -0,0 +1,35 @@
+const title1 = document.createElement ('h3');
+
+title1.textContent = 'Travelling With Your Dog';
+
+document.body.appendChild (title1);
+
+const paragraph = document.createElement ('p');
+
+paragraph.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed eros elit. Donec nisi ex, varius eget tortor molestie, vulputate aliquet justo. Fusce eu fringilla diam. Suspendisse interdum dictum mauris, eu bibendum nisi ullamcorper ac. Vivamus vestibulum ligula nec urna condimentum, non vehicula quam sagittis. Vestibulum congue varius enim quis elementum. Nulla finibus urna turpis, in pellentesque dui iaculis sit amet. Aliquam placerat neque quis odio venenatis egestas. Quisque fringilla eros eget quam accumsan, quis tempor metus rhoncus. Pellentesque nec lectus ut libero iaculis varius. Fusce nunc mauris, tincclassunt vel nulla vitae, dapibus lacinia quam. Integer at facilisis sapien. Etiam nec volutpat dui.';
+
+document.body.appendChild (paragraph);
+
+const title2 = document.createElement ('h3');
+
+title2.textContent = 'How To Walk Multiple Dogs';
+
+document.body.appendChild (title2);
+
+const paragraph2 = document.createElement ('p');
+
+paragraph2.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed eros elit. Donec nisi ex, varius eget tortor molestie, vulputate aliquet justo. Fusce eu fringilla diam. Suspendisse interdum dictum mauris, eu bibendum nisi ullamcorper ac. Vivamus vestibulum ligula nec urna condimentum, non vehicula quam sagittis. Vestibulum congue varius enim quis elementum. Nulla finibus urna turpis, in pellentesque dui iaculis sit amet. Aliquam placerat neque quis odio venenatis egestas. Quisque fringilla eros eget quam accumsan, quis tempor metus rhoncus. Pellentesque nec lectus ut libero iaculis varius. Fusce nunc mauris, tincclassunt vel nulla vitae, dapibus lacinia quam. Integer at facilisis sapien. Etiam nec volutpat dui.';
+
+document.body.appendChild (paragraph2);
+
+const title3 = document.createElement ('h3');
+
+title3.textContent = 'Teach Your Dog To Fetch';
+
+document.body.appendChild (title3);
+
+const paragraph3 = document.createElement ('p');
+
+paragraph3.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sed eros elit. Donec nisi ex, varius eget tortor molestie, vulputate aliquet justo. Fusce eu fringilla diam. Suspendisse interdum dictum mauris, eu bibendum nisi ullamcorper ac. Vivamus vestibulum ligula nec urna condimentum, non vehicula quam sagittis. Vestibulum congue varius enim quis elementum. Nulla finibus urna turpis, in pellentesque dui iaculis sit amet. Aliquam placerat neque quis odio venenatis egestas. Quisque fringilla eros eget quam accumsan, quis tempor metus rhoncus. Pellentesque nec lectus ut libero iaculis varius. Fusce nunc mauris, tincclassunt vel nulla vitae, dapibus lacinia quam. Integer at facilisis sapien. Etiam nec volutpat dui.';
+
+document.body.appendChild (paragraph3);
