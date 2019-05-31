cb70741fdfdfae94da5314253efa2eb172b8ea12
Cwest <46390605+caitlinwest10@users.noreply.github.com>
2019-05-30 19:22:44 -0500

    Refactoring form submission with jquery

--- jquery.js
+++ jquery.js
@@ -5,3 +5,13 @@ $('.card img').mouseover(function () {
     $(this).css('opacity', '0.8');
 
 });
+
+
+$(function () {
+    $('#adoptionForm').on('submit', function (e) {
+        e.preventDefault();
+        var data = $("#adoptionForm :input").serializeArray();
+        console.log(data);
+        alert('Thank you! The form information has been received.');
+    });
+});
