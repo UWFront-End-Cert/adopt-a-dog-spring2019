4cab1f28fbbce90c91487b2bbaa80ef7a9bc9e06
chanjessica <jchan_sea@yahoo.com>
2019-05-31 06:14:34 +0000

    refactor the form console log using jquery

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,19 +1,14 @@
 function sConsole(event) {
     alert('Thank you. The form information has been received.');
     event.preventDefault();
-    console.log ( 'name   : ' + document.getElementById('name').value );
-	console.log ( 'email  : ' + document.getElementById('email').value );  
-	console.log ( 'address: ' + document.getElementById('address').value );
-	console.log ( 'city   : ' + document.getElementById('city').value );
-	console.log ( 'state  : ' + document.getElementById('state').value );
-	console.log ( 'zip    : ' + document.getElementById('zip').value );
-    console.log ( 'Pickup location    : ' + document.getElementById('loc').value );
+
+    console.log ( 'name   : ' + $('input:text').val() );   
+	console.log ( 'email  : ' + $('#email').val() );  
+	console.log ( 'address: ' + $('#address').val() );
+	console.log ( 'city   : ' + $('#city').val() );
+	console.log ( 'state  : ' + $('#state').val() );
+	console.log ( 'zip    : ' + $('#zip').val() );
+    console.log ( 'Pickup location    : ' + $('#loc').val() );s
     console.log ( 'First time adopted?') ;
-    if (document.getElementById('firstYes').checked) {
-        console.log(document.getElementById('firstYes').value);
-        }
-    else if (document.getElementById('firstNo').checked) {
-        console.log(document.getElementById('firstNo').value);
-        };   
-  }
- 
+	console.log ( $('input[type="radio"]:checked').val() );	
+}
