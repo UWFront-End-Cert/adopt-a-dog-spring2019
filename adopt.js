856a7c94d0e57e52a3741d99a7f0fea6a440617d
georgeme <georgeme@uw.edu>
2019-05-31 14:46:45 -0700

    Updates to cart total

--- adopt.js
+++ adopt.js
@@ -17,7 +17,7 @@ function addCost(buttonEl) {
 
   totalPrice = totalPrice + parseFloat(amountEl.innerHTML);
 
-    $('#cart-total').html(' ' + '$' + totalPrice);
+    $('#cart-total').html(' ' + 'Cart: ' + '$' + totalPrice);
 
     alert('Your cart total is: ' + '$' + parseFloat(totalPrice));
 }
