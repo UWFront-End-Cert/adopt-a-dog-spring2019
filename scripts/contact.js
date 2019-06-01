5725877b6343401525a7c89ce0db7ad8a5cf4777
chanjessica <jchan_sea@yahoo.com>
2019-06-01 03:45:29 +0000

    console log using jQuery

---
+++ scripts/contact.js
@@ -0,0 +1,14 @@
+function sConsole(event) {
+    alert('Thank you. The form information has been received.');
+    event.preventDefault();
+
+    console.log ( 'name   : ' + $('input:text').val() );   
+    console.log ( 'email  : ' + $('#email').val() );  
+    console.log ( 'address: ' + $('#address').val() );
+    console.log ( 'city   : ' + $('#city').val() );
+    console.log ( 'state  : ' + $('#state').val() );
+    console.log ( 'zip    : ' + $('#zip').val() );
+    console.log ( 'Pickup location    : ' + $('#loc').val() );
+    console.log ( 'First time adopted?') ;
+    console.log ( $('input[type="radio"]:checked').val() );
+}
