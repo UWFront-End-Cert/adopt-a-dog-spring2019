41c6fb1bbe49cd80820f03723a27fdb5fb9a8c7a
Steve Fountain (HARMAN Connected Services) <v-sfoun@microsoft.com>
2019-05-31 18:37:19 -0700

    jquery checkout page start

--- javascript/scripts-checkout.js
+++ javascript/scripts-checkout.js
@@ -1,75 +1,76 @@
-//checkout page
+        //checkout page
 
-const form = document.querySelector('form');
-const submitButton = document.createElement('button');
-submitButton.setAttribute('type', 'submit');
-submitButton.setAttribute('class', 'adopt-button-checkout');
+// const form = document.querySelector('form');
+// const submitButton = document.createElement('button');
+// submitButton.setAttribute('type', 'submit');
+// submitButton.setAttribute('class', 'adopt-button-checkout');
 
-submitButton.textContent = 'Submit';
-form.appendChild(submitButton);
+// submitButton.textContent = 'Submit';
+// form.appendChild(submitButton);
 
-//runs once form is submitted
-form.addEventListener('submit', function(event) {
+        //runs once form is submitted
 
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const address = document.getElementById('address');
-const city = document.getElementById('city');
-const state = document.getElementById('state');
-const zip = document.getElementById('zip');
-const adopter = adopt();
-const pickupLocation = document.getElementById('pickup-location');
+// form.addEventListener('submit', function(event) {
 
-//when form is submitted, this 
-function adopt() {
-    if (document.getElementById('yes-first').checked) {
-        return document.getElementById('yes-first');
-      }
-    if (document.getElementById('no-first').checked) {
-        return document.getElementById('no-first');
-    } 
-    else return 'null';
-}
+// const name = document.getElementById('name');
+// const email = document.getElementById('email');
+// const address = document.getElementById('address');
+// const city = document.getElementById('city');
+// const state = document.getElementById('state');
+// const zip = document.getElementById('zip');
+// const adopter = adopt();
+// const pickupLocation = document.getElementById('pickup-location');
 
-let customerInfo = [
-    {
-        title: 'name',
-        value: name.value
-    },
-    {
-        title: 'email',
-        value: email.value
-    },
-    {
-        title: 'address',
-        value: address.value
-    },
-    {
-        title: 'city',
-        value: city.value
-    },
-    {
-        title: 'state',
-        value: state.value
-    },
-    {
-        title: 'zip',
-        value: zip.value
-    },
-    {
-        title: 'first time adopter?',
-        value: adopter.value
-    },
-    {
-        title: 'pickup location?',
-        value: pickupLocation.value
-    }
-];
+// //when form is submitted, this 
+// function adopt() {
+//     if (document.getElementById('yes-first').checked) {
+//         return document.getElementById('yes-first');
+//       }
+//     if (document.getElementById('no-first').checked) {
+//         return document.getElementById('no-first');
+//     } 
+//     else return 'null';
+// }
 
-    event.preventDefault();
-    for ( i = 0; i < customerInfo.length; i +=1) {
-        console.log(customerInfo[i].title + ': ' + customerInfo[i].value);
-    }
-    alert('Thank you. The form information has been received');
+// let customerInfo = [
+//     {
+//         title: 'name',
+//         value: name.value
+//     },
+//     {
+//         title: 'email',
+//         value: email.value
+//     },
+//     {
+//         title: 'address',
+//         value: address.value
+//     },
+//     {
+//         title: 'city',
+//         value: city.value
+//     },
+//     {
+//         title: 'state',
+//         value: state.value
+//     },
+//     {
+//         title: 'zip',
+//         value: zip.value
+//     },
+//     {
+//         title: 'first time adopter?',
+//         value: adopter.value
+//     },
+//     {
+//         title: 'pickup location?',
+//         value: pickupLocation.value
+//     }
+// ];
 
-});
+//     event.preventDefault();
+//     for ( i = 0; i < customerInfo.length; i +=1) {
+//         console.log(customerInfo[i].title + ': ' + customerInfo[i].value);
+//     }
+//     alert('Thank you. The form information has been received');
+
+// });
--- libraries/jquery.js
+++ libraries/jquery.js
@@ -7,15 +7,35 @@ $(function(){
         $('p.checkout-total').text('$' + cartTotal.toFixed(2));
     });
 
-    $('.gallery-item').mouseover(function(){
+    $('.gallery-item').hover(function(){
         $(this).css('transition', 'transform .5s')
         $(this).css('transform', 'scale(1.3)')
         $(this).css('z-index', '9')
         $(this).css('backgroundColor', 'white')
-    });
-    $('.gallery-item').mouseleave(function(){
+    },
+    function(){
         $(this).css('transition', 'transform .5s')
         $(this).css('transform', 'scale(1)')
         $(this).css('z-index', 'auto')
     });
+
   });
+
+
+
+
+  //checkout page
+
+$('form').append('<button class="adopt-button-checkout" type="button">Submit</button>');
+
+let inputValues = $('input').find('*').val();
+
+$('button').on("click", function() {
+    event.preventDefault();
+    console.log($('input').find('*').val());
+    console.log(inputValues);
+
+    console.log($('form').serializeArray());
+
+});
+
