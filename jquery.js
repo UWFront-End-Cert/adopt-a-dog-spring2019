0aff600c426a9d24b35654a3f3ed2a093eb9fd50
Cwest <46390605+caitlinwest10@users.noreply.github.com>
2019-06-01 09:49:34 -0500

    Adding total price to header

--- jquery.js
+++ jquery.js
@@ -7,11 +7,35 @@ $('.card img').mouseover(function () {
 });
 
 
+
 $(function () {
     $('#adoptionForm').on('submit', function (e) {
         e.preventDefault();
         var data = $("#adoptionForm :input").serializeArray();
         console.log(data);
         alert('Thank you! The form information has been received.');
+    });
+});
+
+
+
+let adoptionFee = 123.45;
+
+let totalPrice = 0;
+
+function dogClick(data) {
+    alert(data);
+}
+
+function adoptionClick() {
+
+    totalPrice = adoptionFee + totalPrice;
+
+    alert("You have added a new forever friend to your basket. Your total adoption is $" + totalPrice.toFixed(2));
+}
+
+$(document).ready(function () {
+    $('.card-button').click(function () {
+        $('#checkoutTotal').html('$' + totalPrice.toFixed(2));
     });
 });
