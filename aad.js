c1d5361fccd62669dec80c1615d91f48815126a4
DAW <madronadan@gmail.com>
2019-05-31 17:19:25 -0700

    jQuery, cart counting, zoom in

--- aad.js
+++ aad.js
@@ -3,6 +3,19 @@ function info(name, breed) {
     alert(result);
   }
 
+const numberToDisplay = $( "#displayTotal" )[0];
+
+let count = 0;
+
+numberToDisplay.innerText = 0;
+
+function addTotal() {
+  count = count + 123.45;
+  numberToDisplay.innerText = count;
+}
+
+
+/*I removed the pop-up window with the total once we added the running cart total in the top right
 let total = 0;
   
 function adder(value) {
@@ -11,23 +24,32 @@ function adder(value) {
     total += value;
     alert(total)
 }
+*/
 
 function thankYou() {
   event.preventDefault();
-  let name = document.getElementById("name").value;
-  let email = document.getElementById("email").value;
-  let street = document.getElementById("street").value;
-  let city = document.getElementById("city").value;
-  let state = document.getElementById("state").value;
-  let zip = document.getElementById("zip").value;
-  let pickup = document.getElementById("pickup").value;
-  let myArr = [name, email, street, city, state, zip, pickup]
-  console.log(myArr);
+  let name = $('#name').val();
+  let email = $("#email").val();
+  let street = $("#street").val();
+  let city = $("#city").val();
+  let state = $("#state").val();
+  let zip = $("#zip").val();
+  let pickup = $("#pickup").val();
   const formDataContacts = document.querySelector('form').elements.firstadopt;
+
   for (var i = 0; i < formDataContacts.length; i++) {
-    if (formDataContacts[i].checked) {
-      console.log(formDataContacts[i].value);
+      if (formDataContacts[i].checked);
     }
-  }
+
+  let myArr = [name, email, street, city, state, zip, formDataContacts[i].value, pickup]
+  console.log(myArr);
+  
+  
   alert("Thank you. The form information has been received.");
 }
+
+$ ('.profile-card').hover(function(e) {
+  $('img', this).addClass('hover-picture');
+})
+
+ //$('#name') is correctly catching that field. I can turn it pink.
