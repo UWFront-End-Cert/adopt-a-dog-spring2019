7a1880d6bea49c1044f4434ab2fb1bed1ca24ed2
ojspoko <ojspoko@gmail.com>
2019-06-02 09:20:55 +0200

    js/jquery for lesson 9

--- javascript.js
+++ javascript.js
@@ -1,31 +1,33 @@
-function dogInfo () {
-    alert();
+function dogInfo (name) {
+    let result = name;
+    alert(result);
   }
 
+const cartTotal = $('#number')[0];
 let total = 0;
 
-  function adder(value) {
-    let cost = 0;
-    cost += value;
-    total += value;
-    alert('Grand total: ' + '$' + total)
+  cartTotal.innerText = '$' + 0;
+
+function adder() {
+  total = total + 123.45;
+  cartTotal.innerText = '$' + total;
   }
-
-let form = document.forms[0];
-
-function formValues () {
-      console.log(document.getElementById('name').value);
-      console.log(document.getElementById('email').value);
-      console.log(document.getElementById('address').value);
-      console.log(document.getElementById('city').value);
-      console.log(document.getElementById('state').value);
-      console.log(document.getElementById('zip').value);
-  }
-
-form.addEventListener('submit', function(e) {
-     e.preventDefault();
-   });
 
 function submitForm() {
-  alert('Thank you. The form information has been received');
-  }
+  event.preventDefault();
+  let name = $('#name').val();
+  let email = $("#email").val();
+  let address = $("#address").val();
+  let city = $("#city").val();
+  let state = $("#state").val();
+  let zip = $("#zip").val();
+
+let submissions = [name, email, address, city, state, zip]
+console.log(submissions);
+
+alert('Thank you. The form information has been received');
+};
+
+$('.profile').hover(function(e) {
+  $('img',this).toggleClass('box-shadow');
+  });
