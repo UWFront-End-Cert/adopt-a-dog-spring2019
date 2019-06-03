b82641da19f63fe117d7bc06b6d6bc9e61d92c2b
Katie Martell <kaitlym.martell@gmail.com>
2019-06-02 17:10:24 -0700

    implemented jquery, added spin effect on hover of dog tiles, updated checkout total, and logged checkout form data

--- js/checkout.js
+++ js/checkout.js
@@ -1,3 +1,19 @@
 function displayThankYou() {
   alert('Thank you. The form information has been received');
 }
+
+function getFormData() {
+  let formFields = $('.input').serializeArray();
+  let formData = '';
+  $.each(formFields, function(i, field) {
+    console.log(field.name + ': ' + field.value);
+  })
+}
+
+$(document).ready(function() {
+  $('#submit-button').click(function(e) {
+    displayThankYou();
+    getFormData();
+    e.preventDefault();
+  });
+});
--- js/dog-info.js
+++ js/dog-info.js
@@ -1,10 +1,36 @@
-let total = 0;
+let runningTotal = 0;
 
 function alertDogInfo(name, breed, fee) {
     alert(name + ', ' + breed + ', $' + fee);
 }
 
 function addToTotal(fee) {
-    total += fee;
-    alert('Running total: $' + total);
+    runningTotal += fee;
+    alert('Running total: $' + runningTotal);
 }
+
+function updateCheckoutTotal() {
+  $('#checkout-total').text('$' + runningTotal);
+}
+
+$(document).ready(function() {
+  // Add an effect to dog tiles on hover
+  $('.dog').hover(
+    function() {
+      $(this).find('img').css({
+        'transform': 'rotate(360deg)'
+      });
+    },
+    function() {
+      $(this).find('img').css({
+        'transform': 'none'
+      });
+    }
+  );
+
+  // Update checkout total on button clicks
+  $('.dog-button').click(function(e) {
+    addToTotal(parseFloat($(this).attr('price')));
+    updateCheckoutTotal();
+  });
+});
