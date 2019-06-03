bac6b37d1a903c33dbeac2407bb7a77052b1766f
Daniel C Martinez <dcmar18@uw.edu>
2019-06-03 16:30:04 -0700

    Updated code with click handlers on images of dogs and buttons

---
+++ checkout.js
@@ -0,0 +1,43 @@
+let total = 0;
+
+function addTocart() {
+  total = 123.45 + total;
+  alert(total)
+}
+
+function murphy_dog() {
+  alert('Murphy, Beagle, $123.45')
+}
+function poppy_dog() {
+  alert('Poppy, Maltese, $123.45')
+}
+function jack_dog() {
+  alert('Jack, Beagle, $123.45')
+}
+function duffy_dog() {
+  alert('Duffy, Pitbull, $123.45')
+}
+function lucas_dog() {
+  alert('Lucas, Belgian Sheepdog, $123.45')
+}
+function jake_dog() {
+  alert('Jake, Labrador, $123.45')
+}
+function angus_dog() {
+alert('Angus, Boxer, $123.45')
+}
+function violet_dog() {
+  alert('Violet, Labrador, $123.45')
+}
+function piper_dog() {
+  alert('Piper, Retriever, $123.45')
+}
+function maximus_dog() {
+  alert('Maximus, Schnauzer, $123.45')
+}
+function luna_dog() {
+  alert('Luna, Labrador, $123.45')
+}
+function stella_dog() {
+  alert('Stella, Chihuahua, $123.45')
+}
