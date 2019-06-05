cdf23b37b0953b1b71b7b76344edb8ccfa1f64f0
Daniel Scheid <Mac@Daniels-MacBook-Pro-3.local>
2019-06-04 18:23:47 -0700

    Fixed i++

--- checkout.js
+++ checkout.js
@@ -4,7 +4,7 @@ $( "form" ).submit(function(e) {
   e.preventDefault();
   alert("Thank you. The form information has been received");
   var divs = $('input');
-  for (let i = 0; i < divs.length; ++i) {
+  for (let i = 0; i < divs.length; i++) {
     console.log(divs[i].value);
   }
 });
