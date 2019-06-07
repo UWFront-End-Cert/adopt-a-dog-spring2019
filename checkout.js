8a5f15ead1588fbb9e8fc88c4560e96259ab5e49
Daniel Martinez <dcmar18@gmail.com>
2019-06-07 13:55:46 -0700

    Made site more responsive by changing the number of columns for dog grid depending on the size of the screen. Also updated js file to pass in parameters into function

--- checkout.js
+++ checkout.js
@@ -5,39 +5,6 @@ function addTocart() {
   alert(total)
 }
 
-function murphy_dog() {
-  alert('Murphy, Beagle, $123.45')
-}
-function poppy_dog() {
-  alert('Poppy, Maltese, $123.45')
-}
-function jack_dog() {
-  alert('Jack, Beagle, $123.45')
-}
-function duffy_dog() {
-  alert('Duffy, Pitbull, $123.45')
-}
-function lucas_dog() {
-  alert('Lucas, Belgian Sheepdog, $123.45')
-}
-function jake_dog() {
-  alert('Jake, Labrador, $123.45')
-}
-function angus_dog() {
-alert('Angus, Boxer, $123.45')
-}
-function violet_dog() {
-  alert('Violet, Labrador, $123.45')
-}
-function piper_dog() {
-  alert('Piper, Retriever, $123.45')
-}
-function maximus_dog() {
-  alert('Maximus, Schnauzer, $123.45')
-}
-function luna_dog() {
-  alert('Luna, Labrador, $123.45')
-}
-function stella_dog() {
-  alert('Stella, Chihuahua, $123.45')
+function dog_info(dog_name, dog_breed, cost) {
+  alert(dog_name + ", " + dog_breed + ", " + cost)
 }
