d50d727eb7984f03c5cfc5ecdfdf381889644334
Nataly <njbp92@gmail.com>
2019-06-14 01:00:45 -0700

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -8,49 +8,53 @@ function addAdopt() {
   alert(result);
 }
 
-	$('.dog').addClass('.enlarge:hover');
-
-  var formData = new FormData(document.querySelector('AdoptMe'))
+$(".dog").addClass("enlarge");
 
 
-const blogdiv = [
+var FormData = new FormData("document.querySelector('AdoptMe')");
+
+
+
+const newblog = [
   {
-    header: 'Traveling with your Dog',
-    blog: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas. Purus non enim praesent elementum facilisis leo vel. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean. Mauris sit amet massa vitae tortor condimentum. Tellus in hac habitasse platea dictumst. Tortor aliquam nulla facilisi cras fermentum. In dictum non consectetur a erat nam at. Nunc faucibus a pellentesque sit amet porttitor eget. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Duis ultricies lacus sed turpis tincidunt id. Ipsum a arcu cursus vitae congue. Ipsum consequat nisl vel pretium lectus. A diam maecenas sed enim ut sem viverra. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Dictum non consectetur a erat nam at lectus urna. Et molestie ac feugiat sed lectus vestibulum mattis. Scelerisque fermentum dui faucibus in. At imperdiet dui accumsan sit amet nulla facilisi morbi. Dictumst quisque sagittis purus sit amet volutpat consequat. Sagittis orci a scelerisque purus semper. Scelerisque varius morbi enim nunc faucibus a. Commodo elit at imperdiet dui accumsan sit amet nulla. Morbi quis commodo odio aenean sed adipiscing diam.'
-    imageLink: './images/blog-1.jpg'
+    header: "Traveling with your Dog",
+    blogtext: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas. Purus non enim praesent elementum facilisis leo vel. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean. Mauris sit amet massa vitae tortor condimentum. Tellus in hac habitasse platea dictumst. Tortor aliquam nulla facilisi cras fermentum. In dictum non consectetur a erat nam at. Nunc faucibus a pellentesque sit amet porttitor eget. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Duis ultricies lacus sed turpis tincidunt id. Ipsum a arcu cursus vitae congue. Ipsum consequat nisl vel pretium lectus. A diam maecenas sed enim ut sem viverra. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Dictum non consectetur a erat nam at lectus urna. Et molestie ac feugiat sed lectus vestibulum mattis. Scelerisque fermentum dui faucibus in. At imperdiet dui accumsan sit amet nulla facilisi morbi. Dictumst quisque sagittis purus sit amet volutpat consequat. Sagittis orci a scelerisque purus semper. Scelerisque varius morbi enim nunc faucibus a. Commodo elit at imperdiet dui accumsan sit amet nulla. Morbi quis commodo odio aenean sed adipiscing diam.",
+    imageLink: "images/blog-1.jpg"
   },
   {
-    header: 'How to Walk Multiple Dogs',
-    blog: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas. Purus non enim praesent elementum facilisis leo vel. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean. Mauris sit amet massa vitae tortor condimentum. Tellus in hac habitasse platea dictumst. Tortor aliquam nulla facilisi cras fermentum. In dictum non consectetur a erat nam at. Nunc faucibus a pellentesque sit amet porttitor eget. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Duis ultricies lacus sed turpis tincidunt id. Ipsum a arcu cursus vitae congue. Ipsum consequat nisl vel pretium lectus. A diam maecenas sed enim ut sem viverra. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Dictum non consectetur a erat nam at lectus urna. Et molestie ac feugiat sed lectus vestibulum mattis. Scelerisque fermentum dui faucibus in. At imperdiet dui accumsan sit amet nulla facilisi morbi. Dictumst quisque sagittis purus sit amet volutpat consequat. Sagittis orci a scelerisque purus semper. Scelerisque varius morbi enim nunc faucibus a. Commodo elit at imperdiet dui accumsan sit amet nulla. Morbi quis commodo odio aenean sed adipiscing diam.'
-    imageLink: './images/blog-2.jpg'
+    header: "How to Walk Multiple Dogs",
+    blogtext: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas. Purus non enim praesent elementum facilisis leo vel. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean. Mauris sit amet massa vitae tortor condimentum. Tellus in hac habitasse platea dictumst. Tortor aliquam nulla facilisi cras fermentum. In dictum non consectetur a erat nam at. Nunc faucibus a pellentesque sit amet porttitor eget. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Duis ultricies lacus sed turpis tincidunt id. Ipsum a arcu cursus vitae congue. Ipsum consequat nisl vel pretium lectus. A diam maecenas sed enim ut sem viverra. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Dictum non consectetur a erat nam at lectus urna. Et molestie ac feugiat sed lectus vestibulum mattis. Scelerisque fermentum dui faucibus in. At imperdiet dui accumsan sit amet nulla facilisi morbi. Dictumst quisque sagittis purus sit amet volutpat consequat. Sagittis orci a scelerisque purus semper. Scelerisque varius morbi enim nunc faucibus a. Commodo elit at imperdiet dui accumsan sit amet nulla. Morbi quis commodo odio aenean sed adipiscing diam.",
+    imageLink:"images/blog-2.jpg"
   },
   {
-    header: 'Teach Your Dog To Fetch',
-    blog: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas. Purus non enim praesent elementum facilisis leo vel. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean. Mauris sit amet massa vitae tortor condimentum. Tellus in hac habitasse platea dictumst. Tortor aliquam nulla facilisi cras fermentum. In dictum non consectetur a erat nam at. Nunc faucibus a pellentesque sit amet porttitor eget. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Duis ultricies lacus sed turpis tincidunt id. Ipsum a arcu cursus vitae congue. Ipsum consequat nisl vel pretium lectus. A diam maecenas sed enim ut sem viverra. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Dictum non consectetur a erat nam at lectus urna. Et molestie ac feugiat sed lectus vestibulum mattis. Scelerisque fermentum dui faucibus in. At imperdiet dui accumsan sit amet nulla facilisi morbi. Dictumst quisque sagittis purus sit amet volutpat consequat. Sagittis orci a scelerisque purus semper. Scelerisque varius morbi enim nunc faucibus a. Commodo elit at imperdiet dui accumsan sit amet nulla. Morbi quis commodo odio aenean sed adipiscing diam.'
-    imageLink: './images/blog-3.jpg'
+    header: "Teach Your Dog To Fetch",
+    blogtext: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Morbi tincidunt ornare massa eget egestas. Purus non enim praesent elementum facilisis leo vel. Ultricies mi eget mauris pharetra et ultrices neque ornare aenean. Mauris sit amet massa vitae tortor condimentum. Tellus in hac habitasse platea dictumst. Tortor aliquam nulla facilisi cras fermentum. In dictum non consectetur a erat nam at. Nunc faucibus a pellentesque sit amet porttitor eget. Ipsum dolor sit amet consectetur adipiscing elit ut aliquam purus. Duis ultricies lacus sed turpis tincidunt id. Ipsum a arcu cursus vitae congue. Ipsum consequat nisl vel pretium lectus. A diam maecenas sed enim ut sem viverra. Nulla facilisi nullam vehicula ipsum a arcu cursus vitae. Dictum non consectetur a erat nam at lectus urna. Et molestie ac feugiat sed lectus vestibulum mattis. Scelerisque fermentum dui faucibus in. At imperdiet dui accumsan sit amet nulla facilisi morbi. Dictumst quisque sagittis purus sit amet volutpat consequat. Sagittis orci a scelerisque purus semper. Scelerisque varius morbi enim nunc faucibus a. Commodo elit at imperdiet dui accumsan sit amet nulla. Morbi quis commodo odio aenean sed adipiscing diam.",
+    imageLink: "images/blog-3.jpg"
   }
 ];
 
-const adoptadogblog= document.getElementById('blogsec');
-
-blogdiv.forEach(function(character) {
-  const newCharacter = document.createElement('div');
-  blogdiv.setAttribute('class', 'blogdiv');
+const newblogSelection = document.getElementById("blogsec");
 
 
-  const image = document.createElement('img');
-  image.setAttribute('src', ./images/blog-3.jpg);
-  blogdiv.appendChild(image);
+newblog.forEach(function(bdiv) {
 
-  const headline = document.createElement('h2');
-  headline.innerText = bdiv.header;
-  blogdiv.appendChild(headline);
-
-  const job = document.createElement('h2');
-  text.innerText = bdiv.blog;
-  blogdiv.appendChild(job);
+  const  blogdiv = document.createElement("bdiv");
+  blogdiv.setAttribute("class", "blogdiv");
 
 
+  const blogpic = document.createElement("imageLink");
+  blogpic.setAttribute("src", newblog.imageLink);
+  blogsec.appendChild(blogpic);
 
-el.appendChild(adoptadogblog);
+
+  const  header = document.createElement("h1");
+  header.innerText = newblog.header;
+  blogsec.appendChild(blogdiv);
+
+
+  const  blogtext = document.createElement("text");
+  blogtext.innerText = newblog.blogtext;
+  blogsec.appendChild(blogdiv);
+
+newblogSelection.appendChild(blogdiv);
 });
